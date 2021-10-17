78283a6612df269091b34c853400f09fe2724123
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-10-17 13:09:18 -0400

    Separate the styling of the input to a different component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game!</Text>
 
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select A Number</Text>
 
                 <TextInput />
@@ -15,7 +16,7 @@ const StartGameScreen = props => {
                     <Button title="Reset" onPress={() => {}} />
                     <Button title="Confirm" onPress={() => {}} />
                 </View>
-            </View>
+            </Card>
         </View>
     )
 }
@@ -35,15 +36,7 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        backgroundColor: 'white',
-        elevation: 8,
-        padding: 20,
-        borderRadius: 10
+        alignItems: 'center' 
     },
     buttonContainer: {
         flexDirection: 'row',
