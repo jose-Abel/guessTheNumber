37b8be57f3e4e02bb1f5258568963b9c34ae9234
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-10-18 13:49:38 -0400

    Create the Input component, colors constants and display on the StartGameScreen

--- components/Card.js
+++ components/Card.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-const Card = (props) => {
+const Card = props => {
   return (
-    <View style={{...styles.card, ...props.style}}>
-      {props.children} 
-    </View>
-  )
-}
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
 
 const styles = StyleSheet.create({
-  card: { 
+  card: {
     shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2}, 
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.26,
-    backgroundColor: 'white',
     elevation: 8,
-    padding: 20, 
+    backgroundColor: 'white',
+    padding: 20,
     borderRadius: 10
-   }
-})
+  }
+});
 
 export default Card;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,49 +1,68 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
 import Card from '../components/Card';
+import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game!</Text>
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a New Game!</Text>
 
-            <Card style={styles.inputContainer}>
-                <Text>Select A Number</Text>
+      <Card style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <Input
+            blurOnSubmit autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="numeric"
+            maxLength={2}
+            style={styles.input}/>
 
-                <TextInput />
+        <View style={styles.buttonContainer}>
 
-                <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={() => {}} />
-                    <Button title="Confirm" onPress={() => {}} />
-                </View>
-            </Card>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+          </View>
+
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+          </View>
+
         </View>
-    )
-}
+      </Card>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        padding: 10,
-        alignItems: 'center',
-        justifyContent: "flex-start"
-    },
-    title: {
-        fontSize: 20,
-        marginVertical: 10,
-        
-    },
-    inputContainer: {
-        width: 300,
-        maxWidth: '80%',
-        alignItems: 'center' 
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'space-between',
-        paddingHorizontal: 15
-    }
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 5
+  },
+  button: {
+    width: 80
+  },
+  input: {
+      width: 50,
+      textAlign: 'center'
+  }
 });
 
 export default StartGameScreen;
